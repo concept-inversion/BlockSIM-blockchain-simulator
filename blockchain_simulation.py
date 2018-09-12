635964789b4602c8f51371ec8617a6684545980b
concept-inversion <santosh.pandey2222@gmail.com>
2018-09-12 09:02:22 +0545

    adds trans and node generator

--- blockchain_simulation.py
+++ blockchain_simulation.py
@@ -1,5 +1,29 @@
 import random
 import simpy
 
-def trans_generator():
+NO_NODES = 20
+MEAN_TRANS_GEN_TIME= 8
+SD_TRANS_GEN_TIME= 2 
+
+def node_generator():
+    global nodeID
+    nodeID= random.sample(range(1000,1000+NO_NODES),NO_NODES)
+
+def trans_generator(env):
+    txID = 0
+    while True:
+        yield env.timeout(random.gauss(MEAN_TRANS_GEN_TIME,SD_TRANS_GEN_TIME))
+        print("Generating transactions at %d"% env.now)
+        txID  += 1
+        node = random.choice(nodeID)
+        print("Assigned transaction %d to node %d " %(txID, node))
+        yield env.timeout(5)
+
+def trans_processor(env):
     pass
+
+node_generator()
+env = simpy.Environment()
+env.process(trans_generator(env))
+env.run(until=30)
+
