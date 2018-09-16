d9c1b1aa31d6c4337efa13ba060d14eff4896637
concept-inversion <santosh.pandey2222@gmail.com>
2018-09-16 22:33:55 +0545

    adds broadcast pipe but doesnt work

--- blockchain_simulation.py
+++ blockchain_simulation.py
@@ -1,55 +1,54 @@
 import random
 import simpy
-from simpy.
 
-NO_NODES = 4
-MEAN_TRANS_GEN_TIME= 30
+NO_NODES = 1
+MEAN_TRANS_GEN_TIME= 5
 SD_TRANS_GEN_TIME= 1 
 MINING_TIME= 2
 BLOCKSIZE= 5
 MEMPOOL_SIZE= 10
 
 class nodes():
-    def __init__(self,nodeID):
+    def __init__(self,nodeID,bc_pipe):
         self.nodeID= nodeID
         self.env= env
         self.task_list = []
         self.mempool_size = 10
         self.block_list= []
+        self.bc_pipe = bc_pipe
         self.res = simpy.Resource(env,capacity=1)
         print("Node generated with node ID: %d " % self.nodeID)
         
-        
+ 
     def run(self):
         yield env.timeout(5)
         print("Running node id %d at time %d"% (self.nodeID,env.now))
     def add_task(self,txID):
         self.task_list.append(txID)
         #print("task number %d added to the node %d " %(txID,self.nodeID))
-        env.process(self.mining())
-
-    def broadcast(self):
-        pass
+        env.process(self.mining(self.bc_pipe))
     
-    def mining(self):
+    def mining(self,bc_pipe):
         # Starts mining/verification of the transactions and handles interrupt for updating the blocks
             if len(self.task_list) != 0:
                 request = self.res.request()
                 yield request
-                task=self.task_list.pop()
+                task=self.task_list.pop(0)
                 print("Mining  | task %d | node %d | time %d " % (task, self.nodeID,env.now))
                 yield env.timeout(13)
+                result=self.bc_pipe.put(task)
+                print(result)
                 self.block_list.append(task)
                 self.res.release(request)
                 print("Completed |  %d | node %d | time %d" %(task,self.nodeID,env.now))
             else:
-                self.mining()
+                self.mining(bc_pipe)
 
-def node_generator(env):
+def node_generator(env,bc_pipe):
     global nodeID
     nodeID= random.sample(range(1000,1000+NO_NODES),NO_NODES)
     global node_map
-    node_map = [nodes(each) for each in nodeID]
+    node_map = [nodes(each,bc_pipe) for each in nodeID]
     #import ipdb; ipdb.set_trace()
     print("%d nodes generated:"% NO_NODES)
 
@@ -65,12 +64,32 @@ def trans_generator(env):
             if i.nodeID==node:
                 i.add_task(txID)
                 #print("Transaction %d appended to the node %d : "%(txID,i.nodeID))
-        yield env.timeout(5)
+
+class Broadcaster():
+    def __init__(self, env,capacity=simpy.core.Infinity):
+        self.env = env
+        self.capacity = capacity
+        self.pipes = []
+        
+    def put(self, value):
+        if not self.pipes:
+            raise RuntimeError('There are no output pipes.')
+        events = [store.put(value) for store in self.pipes]
+        return self.env.all_of(events)
+
+    def get_output_conn(self):
+        pipe = simpy.Store(self.env, capacity=self.capacity)
+        self.pipes.append(pipe)
+        return pipe
+    
 
 if __name__== "__main__":
     env = simpy.Environment()
-    node_generator(env)
+    pipe = simpy.Store(env)
+    bc_pipe = Broadcaster(env)
+    bc_pipe.put("lol")
+    node_generator(env,bc_pipe.pipes)
     env.process(trans_generator(env))
-    for node in node_map:
-        env.process(node.mining())
+    # for node in node_map:
+    #     env.process(node.mining(bc_pipe))
     env.run(until=100)
