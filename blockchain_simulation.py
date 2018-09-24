f20b3b052d046b6327b3788e0605aead0ab7b303
concept-inversion <santosh.pandey2222@gmail.com>
2018-09-24 09:04:17 +0545

    adds node parallel functionality with while true

--- blockchain_simulation.py
+++ blockchain_simulation.py
@@ -7,18 +7,19 @@ SD_TRANS_GEN_TIME= 1
 MINING_TIME= 2
 BLOCKSIZE= 5
 MEMPOOL_SIZE= 10
+BLOCKTIME = 20
 
 class nodes():
     def __init__(self,nodeID,cable):
         self.nodeID= nodeID
         self.env= env
-        self.task_list = []
+        self.mempool= []
         self.mempool_size = 10
         self.block_list= []
         self.cable= cable
-        self.res = simpy.Resource(env,capacity=1)
-        self.process= None
-        #self.process = env.process(self.mining(self.bc_pipe))
+        self.res= simpy.Resource(env,capacity=1)
+        #self.process= None
+        self.process = env.process(self.mining())
         print("Node generated with node ID: %d " % self.nodeID)
         
     def run(self):
@@ -26,24 +27,31 @@ class nodes():
         print("Running node id %d at time %d"% (self.nodeID,env.now))
     
     def add_task(self,txID):
-        self.task_list.append(txID)
-        #print("task number %d added to the node %d " %(txID,self.nodeID))
-        self.process=env.process(self.mining())
+        self.mempool.append(txID)
+        #print("task  %d | added | node %d " %(txID,self.nodeID))
+        #setattr(self,'process',env.process(self.mining()))
     
     def mining(self):
-        # Starts mining/verification of the transactions and handles interrupt for updating the blocks
-            if len(self.task_list) != 0:
-                request = self.res.request()
-                yield request
-                task=self.task_list.pop(0)
-                print("Mining  | task %d | node %d | time %d " % (task, self.nodeID,env.now))
-                yield env.timeout(13)
-                self.cable.put(task,self.nodeID)
-                self.block_list.append(task)
-                self.res.release(request)
-                print("Completed |  %d | node %d | time %d" %(task,self.nodeID,env.now))
-            else:
-                self.mining()
+        '''
+         Starts mining/verification of the transactions and handles interrupt for updating the blocks
+        '''
+        while True:
+            yield env.timeout(BLOCKTIME)   
+            if len(self.mempool) != 0:
+                try:
+                    print("Reading | Mempool | size %d | node %d | time %d."%(len(self.mempool),self.nodeID,env.now))
+                    request = self.res.request()
+                    yield request
+                    task=self.mempool.pop(0)
+                    print("Mining  | task %d | node %d | time %d " % (task, self.nodeID,env.now))
+                    yield env.timeout(13)
+                    self.cable.put(task,self.nodeID)
+                    self.block_list.append(task)
+                    self.res.release(request)
+                    print("Completed |  %d | node %d | time %d" %(task,self.nodeID,env.now))
+                except simpy.Interrupt:
+                    print("%d is interrupted" %self.nodeID)
+
 
 def node_generator(env,cable):
     global nodeID
@@ -59,8 +67,9 @@ def trans_generator(env):
         yield env.timeout(random.gauss(MEAN_TRANS_GEN_TIME,SD_TRANS_GEN_TIME))
         txID  += 1
         print("Generating |  %d  | time %d ."% (txID,env.now))
+        # Choose a node randomly from the nodelist
         node = random.choice(nodeID)
-        #import ipdb; ipdb.set_trace()
+        # Assign the task to the node; Find the node object with the nodeID
         for i in node_map:
             if i.nodeID==node:
                 i.add_task(txID)
@@ -72,20 +81,18 @@ class Network():
         self.delay = 5
         self.store = simpy.Store(env)
 
-    def latency(self, value, nodeID):
+    def latency(self, value, nodeID): 
         yield self.env.timeout(self.delay)
-        print("interrupting by nodeID  %d......." %nodeID)
-        #import ipdb; ipdb.set_trace()
-        for node in node_map:
-            if node.nodeID != nodeID:
-                node.process.interrupt()
-
+        
     def put(self, value, nodeID):
         print("Node %d broadcasted value %d " %(nodeID,value))
         self.env.process(self.latency(value,nodeID))
 
-#Broadcaster Not used due to complexity in modelling the network latency
+
 class Broadcaster():
+    '''
+    Broadcaster Not used due to complexity in modelling the network latency
+    '''
     def __init__(self, env,capacity=simpy.core.Infinity):
         self.env = env
         self.capacity = capacity
@@ -109,10 +116,7 @@ class Broadcaster():
 
 if __name__== "__main__":
     env = simpy.Environment()
-    bc_pipe = Broadcaster(env)
     cable = Network(env)
     node_generator(env,cable)
     env.process(trans_generator(env))
-    # for node in node_map:
-    #     env.process(node.mining(bc_pipe))
-    env.run(until=100)
+    env.run(until=50)
