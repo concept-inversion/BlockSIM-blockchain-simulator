d3351ed632de3e9fefb1283ae8bdbed6900880c7
concept-inversion <santosh.pandey2222@gmail.com>
2018-10-03 11:37:18 +0545

    time added in tasks

--- tasks.py
+++ tasks.py
@@ -1,9 +1,14 @@
+import time
+
 class task():
     def __init__(self,gas,size,id):
         self.gas = gas
         self.size = size
         self.id = id
         self.type = 1
+        self.gen_time = time.ctime()
+        self.appended_time = None
+        self.blocked_time = None
     
     def __repr__(self):
         return str(self.id)
